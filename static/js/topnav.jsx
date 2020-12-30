7aab50f66a47e1e8232d5bbc0da9c7749cb631ff
Angela Purcell <kalmpurcell@gmail.com>
2020-12-30 21:08:08 +0000

    created topnav.jsx

---
+++ static/js/topnav.jsx
@@ -0,0 +1,51 @@
+"use strict";
+
+
+// TODO
+// ! Search OMDB and display the results (movies only)
+
+function TopNav(props){
+
+  const history = useHistory()
+
+  function handleSubmit(evt){
+    evt.preventDefault()
+    localStorage.removeItem('user');
+    props.setUser(undefined)
+    console.log('logged out')
+    history.push('/');
+  }
+
+
+    return(
+        <React.Fragment>
+        <Navbar
+        scrolling="true"
+        expand="sm"
+        fixed='top'
+        id='topnav'>
+
+
+             <Navbar.Brand>
+                 <Link to='/'>
+                    Shoppies
+                </Link>
+            </Navbar.Brand>
+
+
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        {props.user?'':<Nav.Link><Link to="/signup">Login | Signup</Link></Nav.Link>}
+                        {props.user?
+                        <NavDropdown title= {props.user.fname} id="basic-nav-dropdown">
+                                <NavDropdown.Item><Link to="/user-profile">Profile</Link></NavDropdown.Item>
+                                <NavDropdown.Item><Button onClick={handleSubmit} variant="light">Logout</Button></NavDropdown.Item>
+                        </NavDropdown>:''}
+                    </Nav>
+                </Navbar.Collapse>
+        </Navbar>
+
+        </React.Fragment>
+    );
+}
