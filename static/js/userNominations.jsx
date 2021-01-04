0cb8ecc1100700fced006415452df96cfdcd460a
Angela Purcell <kalmpurcell@gmail.com>
2021-01-04 02:02:50 +0000

    made user nominations component

---
+++ static/js/userNominations.jsx
@@ -0,0 +1,46 @@
+
+//  will show this page if user returns and has submitted nominations
+
+function UserNominations(props) {
+
+    const [movies, setMovies] = React.useState([])
+
+    React.useEffect(() => {
+        let data = {'user_id' : 1}
+        fetch('/api/get-user-nominations' ,
+        {method: "POST",  body: JSON.stringify(data),  headers: {'Content-Type': 'application/json'}})
+        .then(response => response.json())
+        .then(data => setMovies(data))
+      }, []);
+
+
+    function generateMovieCards(){
+        const cards = movies.map((movie,index) =>(
+
+            <Card   key={movie.Title + index} value={index}>
+
+                <Card.Img   variant="top"
+                            src={movie.Poster}/>
+
+                <Card.Body>
+
+                <Card.Title>
+                    <div className='truncate-description'>{movie.Title}</div>
+                    <h6><small>{movie.Year}</small></h6>
+                </Card.Title>
+
+              </Card.Body>
+
+            </Card>
+
+        ))
+        return cards
+    }
+
+
+    return (
+
+        {generateMovieCards()}
+
+    );
+}
