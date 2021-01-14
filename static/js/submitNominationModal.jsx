7c4511bed21193e45b802d4709bb561de9b475f6
Angela Purcell <kalmpurcell@gmail.com>
2021-01-14 00:52:08 +0000

    added tweet button and copy to clipboard

--- static/js/submitNominationModal.jsx
+++ static/js/submitNominationModal.jsx
@@ -1,6 +1,9 @@
 function SubmitNominationModal(props) {
-  console.log('submitnominationsmodalprops',props)
-  
+
+  function copyToClipBoard(link){
+    document.execCommand("copy");
+    alert("Copied the text: " + link);
+  }
     return (
       <Modal
         {...props}
@@ -15,9 +18,22 @@ function SubmitNominationModal(props) {
           </p>
         </Modal.Body>
         <Modal.Footer>
-          <div> text here <a>{props.shareableLink}</a></div>
-          <Button onClick={props.onHide}>Share with Friends</Button>
+
+        <Button onClick={() => copyToClipBoard(props.shareableLink)}>
+         Copy link to clipboard
+        </Button>
+
+
+                   <a class="twitter-share-button"
+                    rel="canonical"
+                    href={`https://twitter.com/intent/tweet?text=I%20just%20submit%20nominations%20for%20Shoppies%202021%20awards!%20https%3A%2F%2F${props.shareableLink}%2F`}>
+                  Tweet</a>
+
         </Modal.Footer>
       </Modal>
     );
   }
+
+// sourced from https://codesandbox.io/s/react-hooks-counter-demo-forked-9lsol?file=/src/index.js:103-448
+// https://stackoverflow.com/questions/63546951/react-copy-to-clipboard-using-useref-hook
+
